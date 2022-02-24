3e4157edf95a9a313ae687dd07007a158bdf56c8
Gregory R. Warnes <greg@warnes.net>
2022-02-23 19:29:47 -0500

    stop-bladeRF-wiphy.sh: * Preserve existing value of BUILD_DIR from environment. * Exportt BUILD_DIR to environment * Attempt to determine the correct wlan interface # as the number of existing interfaces (counting from 0). * Use `pkill` instead of killall to terminate programs. * Terminate all launched programs
    start-bladeRF-wiphy.sh
    * Use `x-terminal-emulator` to avoid assumptions about windowing system
    * Unload existing modules before loading the new ones.
    * Start dhcpd in a terminal
    * Check for presence of nmcli before running it.
    * Properly determine detected MAC address.
    * Temporarily disable ip forwarding
    
    install.sh:
    * Use `apt` intead of `apt-get`.
    * Check for Raspberry Pi vs 'standard' debian & use the correct kernel header package.
    * Download standard `hosted` firmware as well as FX2 and wlan firmware.
    * Correct installation of firmware.
    * Limit git clones to depth=1 to avoid unecessarily large downloads.
    * Use correct git branch for Kernel version based on whether VENDOR='Raspbian'
    * Replace some remaining fixed paths with ${BUILD_DIR}

--- bin/start-bladeRF-wiphy.sh
+++ bin/start-bladeRF-wiphy.sh
@@ -1,68 +1,87 @@
 #!/bin/bash
-
+#
 # Script to start up bladeRF-wiphy
-
+#
 set -e          # stop on any error
 set -x          # uncomment to display lines as they are executed
 # trap read debug # uncomment to prompt after each line
 
+# Configuration 
+export BUILD_DIR=${BUILD_DIR:-$HOME/wiphy-build-test}
+
+# Set the correct terminal command
+VENDOR=${VENDOR:-"$(lsb_release -si)"}
+TERMINAL=x-terminal-emulator 
+
+# CLean out any existing kernel modules
+MODULES="^mac80211|^mac80211_hwsim|^bladeRF_mac80211_hwsim"
+lsmod | awk '{print $1}' | egrep "${MODULES}" | xargs -rt sudo rmmod
+
+# Check for existing wlan interfaces so we can use the correct 
+# device name (one higher)
+WLAN_INDEX=$( ip -o link | awk '{print $2}' | egrep '^wlan[0-9]+' | wc -l )
+export DEV=${DEV:-"wlan${WLAN_INDEX}"}
+echo "Using device '${DEV}'"
+
+# Load the FPGA firmware
+# bladeRF-cli -f /usr/share/Nuand/bladeRF/bladeRF_fw_latest.img
+bladeRF-cli -l /usr/share/Nuand/bladeRF/wlanxA9-latest.rbf
+
 # Load the kernel modules
 sudo modprobe cfg80211
 sudo modprobe mac80211 
-sudo modprobe bladeRF_mac80211_hwsim
-
-read -p "Press <RETURN> to contiue." prompt 
+sudo modprobe bladeRF_mac80211_hwsim radios=1
 
 # Prevent network manager from controlling the interface
-if [ $(env nmcli >& /dev/null)]; then    
-    env nmcli && sudo nmcli dev set wlan0 managed false
+if [ $(which nmcli) ]; then    
+    env nmcli && sudo nmcli dev set ${DEV} managed false
 fi
-
 
 # Get the mac address of the physical ethernet adaptor
 MACADDR=$( LANG=C ip -o link show | egrep -v 'wlan|mon|loop' | awk '{print $17}' )
-echo "Using MAC addresss $"
+echo "Using MAC addresss ${MACADDR}"
 
 # Set bladeRF-wiphy MAC address 
-sudo ip link set wlan0 down
-sudo ip link set wlan0 address ${MACADDR}
-sudo ip link set wlan0 up
+sudo ip link set ${DEV} down
+sudo ip link set ${DEV} address ${MACADDR}
+sudo ip link set ${DEV} up
 
 # Congigure 802.11 options
-sudo iw wlan0 set bitrates legacy-2.4 6 9 12 18 24 48 54
+sudo iw ${DEV} set bitrates legacy-2.4 6 9 12 18 24 48 54
 
 # Add monitoring interface
-sudo iw dev wlan0 interface add mon0 type monitor
-sudo ip link set mon0 up
+sudo ip link set ${DEV} down
+sudo iw dev ${DEV} interface add mon0 type monitor
 
 # Set bladeRF-wiphy IP address & properties
 
-sudo ip link set wlan0 down
+sudo iw dev ${DEV} set type monitor
+sudo iw dev ${DEV} set freq 5825
+sudo ip address change 10.254.239.1 dev ${DEV} # !! Be sure this matches the range in dhhcpd.conf
 
-sudo ip address add 10.254.239.1 dev wlan0 # !! Be sure this matches the range in dhhcpd.conf
-sudo iw dev wlan0 set type monitor
-sudo iw dev wlan0 set freq 5825
+sudo ip link set ${DEV} up
+sudo ip link set mon0 up
 
-sudo ip link set wlan0 up
+# # Enable network forwarding
+# echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
 
-# Enable network forwarding
-echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
-
-# Use the `enp0s5` interface for IP forwarding
-sudo iptables -t nat -A POSTROUTING -s 10.254.239.1/16 -o enp0s5 -j MASQUERADE
-
-# Load the FPGA firmware
-bladeRF-cli -f /usr/share/Nuand/bladeRF/bladeRF_fw_latest.img
-bladeRF-cli -l /usr/share/Nuand/bladeRF/wlanxA9-latest.rbf
+# # Forward packages using any/all default route(s)
+# GWDEV=$( ip route show | grep default | awk '{print $5}' )
+# for DEVICE in ${GWDEV}; do
+#     echo "Forwarding packets using ${DEVICE}"
+#     sudo iptables -t nat -A POSTROUTING -s 10.254.239.1/16 -o ${DEVICE} -j MASQUERADE
+# done
 
 # Start all components in separate terminals
-gnome-terminal -t /var/log -- tail -f /var/log/*
-gnome-terminal -t bladeRF-linux-mac80211 -- bladeRF-linux-mac80211
-gnome-terminal -t bladeRF-net.py --working-directory=$HOME/wiphy-build/bladeRF-net -- python3 bladeRF-net.py
-gnome-terminal -t hostapd -- sudo /usr/local/bin/hostapd /etc/hostapd/hostapd.conf
+trap 'read -p "Press <Return> to continue"' debug # uncomment to prompt after each line
+${TERMINAL} -t /var/log -e tail -f /var/log/*.log
+${TERMINAL} -t bladeRF-linux-mac80211 -e /usr/local/bin/bladeRF-linux-mac80211
+${TERMINAL} -t bladeRF-net.py --working-directory=${BUILD_DIR}/bladeRF-net -e python3 bladeRF-net.py
+${TERMINAL} -t dhcpd   -e sudo dhcpd -f 
+${TERMINAL} -t hostapd -e sudo /usr/local/bin/hostapd /etc/hostapd/hostapd.conf
 
-sleep 5s
-sudo service isc-dhcp-server restart
+#sleep 5s
+#sudo service isc-dhcp-server restart
 
 cat <<EOF
 
--- bin/stop-bladeRF-wiphy.sh
+++ bin/stop-bladeRF-wiphy.sh
@@ -6,18 +6,18 @@ set -e          # uncomment to stop on any error
 # set -x          # uncomment to display lines as they are executed
 # trap read debug # uncomment to prompt after each line
 
-# Settings
-DEV="wlan1"
+# Configuration
+export BUILD_DIR=${BUILD_DIR:-$HOME/wiphy-build-test}
 
-sudo killall bladeRF-linux-mac80211 >& /dev/null && true
+# Termeinate the user-mode driver
+sudo pkill -f hostapd && true
+sudo pkill -f bladeRF-linux-mac80211 && true
+sudo pkill -f "python3 bladeRF-net.py" && true
+sudo pkill -f "tail -f /var" && true
 
 # Unload kernel modules
 MODULES="^mac80211|^mac80211_hwsim|^bladeRF_mac80211_hwsim"
 lsmod | awk '{print $1}' | egrep "${MODULES}" | xargs -rt sudo rmmod
-
-# Clean up any remaining devices
-DEVICES="^${DEV}|^bladeRFwlan|^mon"
-ip -o link | awk '{ print $2}' | egrep "${DEVICES}" | xargs -rt -I DEV sudo ip link set DEV down
 
 # Restore standard bladeRF FPGA code
 echo
